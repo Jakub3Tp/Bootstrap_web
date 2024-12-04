c492fe7317f49b959c365179debbc57b131b9213
UTF <ggyarinrblx@gmail.com>
2024-12-04 08:35:30 +0100

    04.12.2024

--- src/App.js
+++ src/App.js
@@ -3,7 +3,46 @@ import "bootstrap/dist/js/bootstrap"
 
 function App() {
   return (
-      <></>
+      <>
+        <header className="App-header">
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-8">
+
+                    </div>
+                    <div className="col-4">
+
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div className="App">
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-3">
+
+                    </div>
+                    <div className="col-5">
+
+                    </div>
+                    <div className="col-4">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+          <footer className="App-footer">
+              <div className="container text-center">
+                  <div className="row">
+                      <div className="col-12">
+
+                      </div>
+                </div>
+            </div>
+        </footer>
+      </>
   )
 }
 
